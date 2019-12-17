d2015238cedc0ce2ce80474bf3f4caf67fa871c3
jym503558564 <503558564@qq.com>
2019-12-17 09:53:06 +0800

    [Update] 抽出TabbarItem

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <div id="app">
-    <tab-bar></tab-bar>
+    <tab-bar>
+      <tabbar-item>
+        <div slot="item-icon" class="iconfont icon-shouye"></div>
+        <div slot="item-text">首页</div>
+      </tabbar-item>
+      <tabbar-item>
+        <div slot="item-icon" class="iconfont icon-fenlei"></div>
+        <div slot="item-text">分类</div>
+      </tabbar-item>
+      <tabbar-item>
+        <div slot="item-icon" class="iconfont icon-gouwuche"></div>
+        <div slot="item-text">购物车</div>
+      </tabbar-item>
+      <tabbar-item>
+        <div slot="item-icon" class="iconfont icon-wode"></div>
+        <div slot="item-text">我的</div>
+      </tabbar-item>
+    </tab-bar>
   </div>
 </template>
 
 <script>
   import TabBar from './components/tabbar/Tabbar'
+  import TabbarItem from "./components/tabbar/TabbarItem";
 
 export default {
   name: 'App',
   components: {
-    TabBar
+    TabBar,
+    TabbarItem
   }
 }
 </script>
---
+++ src/components/tabbar/TabbarItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="tab-bar-item">
+    <slot name="item-icon"></slot>
+    <slot name="item-text"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabbarItem"
+  }
+</script>
+
+<style scoped>
+  @import "./../../assets/css/iconfont.css";
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+  }
+</style>
